7d13e837d8cc2e1713fd4aeaf0a2be4dea7311a9
Daniel Kowalczyk <daniel.kowalczyk1984@gmail.com>
2024-03-31 22:49:07 +0200

    add sha bat

--- pkgs/themes/catppuccin-bat/default.nix
+++ pkgs/themes/catppuccin-bat/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     owner = "catppuccin";
     repo = "bat";
     rev = "b19bea35a85a32294ac4732cad5b0dc6495bed32";
-    hash = "";
+    hash = "sha256-POoW2sEM6jiymbb+W/9DKIjDM1Buu1HAmrNP0yC2JPg=";
   };
 
   installPhase = ''
